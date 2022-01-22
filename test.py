6259a04db36cbcac4274528ecca1a553f990d64f
andgaspar <andrea.gasparin.87@gmail.com>
2022-01-22 12:30:25 +0100

    added curfew fixed for wide body
    to_do: check

--- test.py
+++ test.py
@@ -23,3 +23,30 @@ plt.plot(delays, [cost_fun_mc(d) for d in delays], label="3 p")
 plt.legend()
 plt.show()
 # print(data)
+
+'''
+XXX = 80.000 EUR for the following aircraft types: 77X, 77W, 772, 388, 343
+
+XXX = 40.000 EUR for all other aircraft types
+
+
+77X ???? not found
+
+
+
+cluster = pd.read_csv("CostPackage/Cluster/aircraftClustering.csv")
+cluster_dict = dict(zip(cluster.AircraftType, cluster.AssignedAircraftType))
+import numpy as np
+vals = list(cluster_dict.values())
+v = np.unique(vals)
+for i in v:
+    print(i)
+
+
+cluster["extra"] = cluster.AircraftType.apply(lambda name: name[-3:])
+p = cluster[cluster.extra.isin(['77X', '77W', '772', '388', '343'])]
+
+TURNAROUND TO BE INCLUDED IN THE CURFEW CSV
+EXTEND FIXED COSTS TO OTHER WIDEBODY CLUSTER
+
+'''
